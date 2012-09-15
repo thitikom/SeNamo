23b8a75ad0973100dcfd68d887b300cce9fd0bf8
khemin <nolykk@gmail.com>
2012-09-15 21:39:26 +0700

    add /backoffice (redirect to /backoffice/login in urls.py

--- SeNamo/urls.py
+++ SeNamo/urls.py
@@ -61,6 +61,7 @@ urlpatterns = patterns('',
     url(r'^supplier/(?P<supplier_id>\d+)/delete$', 'app.views.delete_supplier'),
 
     #url for backOffice
+    url(r'^backoffice$',redirect_to, {'url': '/backoffice/login'}),
     url(r'^backoffice/login$','app.backOffice.login'),
     url(r'^backoffice/logout$', 'django.contrib.auth.views.logout',{'next_page':'/backoffice/login'}),
     url(r'^backoffice/managestock$','app.backOffice.managestock'),
